f0ee8bdba59965e8e183330623ac59a5e324cbe1
allenyxu <72947445+allenyxu@users.noreply.github.com>
2021-03-17 11:33:59 -0700

    Add files via upload

---
+++ model.py
@@ -0,0 +1,23 @@
+
+#from flask_login import UserMixin
+
+from db import db
+
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=True)
+    img = db.Column(db.Text, unique=False, nullable=True)
+    filename = db.Column(db.Text, nullable=False)
+    satisfaction = db.Column(db.Text)
+    mimetype = db.Column(db.Text, nullable=False)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text, nullable=False)
+    email = db.Column(db.Text, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+
+
