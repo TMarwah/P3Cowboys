c8e9c343fb768c091edd3bf91ee3eb6367120cc9
Allen Xu <allenyxu@users.noreply.github.com>
2021-03-23 11:22:50 -0700

    upload.html

--- y2021/tri1/app.py
+++ y2021/tri1/app.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from flask import render_template
 
 y2021_tri1_bp = Blueprint('y2021_tri1', __name__,
                           template_folder='templates',
@@ -8,3 +9,7 @@ y2021_tri1_bp = Blueprint('y2021_tri1', __name__,
 @y2021_tri1_bp.route('/')
 def index():
     return "Y2021 tri1 Home Site"
+
+@y2021_tri1_bp.route("/upload")
+def upload():
+    return render_template("/y2021/tri1/upload.html",)
