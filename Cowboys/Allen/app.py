ddf615dd9ea504df8e6813dc17dc2e5f923825ab
marchumeau <72947138+marchumeau@users.noreply.github.com>
2021-03-25 11:04:27 -0700

    Creating a base template for the browse page.

--- Cowboys/Allen/app.py
+++ Cowboys/Allen/app.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
 from flask import render_template
 
+
+backgrounds = ["https://www.teahub.io/photos/full/193-1933361_laptop-aesthetic-wallpapers-anime.jpg"]
+
 Cowboys_Allen_bp = Blueprint('Cowboys_Allen', __name__,
                              template_folder='templates',
                              static_folder='static', static_url_path='assets')
@@ -10,7 +13,14 @@ Cowboys_Allen_bp = Blueprint('Cowboys_Allen', __name__,
 def index():
     return "Y2021 tri1 Home Site"
 
+@Cowboys_Allen_bp.route('/browse')
+def browse():
+    return render_template("browse.html")
 
 @Cowboys_Allen_bp.route("/upload")
 def upload():
     return render_template("upload.html")
+
+
+
+
