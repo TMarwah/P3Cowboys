b0d217905a09bfe11dd9cb03d32ee7b682df726e
TMarwah <44328801+TMarwah@users.noreply.github.com>
2021-04-02 17:10:44 -0700

    fixed counters

--- Cowboys/Tanmay/tanmayminilab.py
+++ Cowboys/Tanmay/tanmayminilab.py
@@ -4,18 +4,15 @@ class Counters:
         self._sentence = sentence
 
     def wordcount(self):
-        i = 0
         input = self._sentence
         input_split = input.split(' ')
-        for word in input_split:
-            i += 1
-
-        return i
+        wordcount = len(input_split)
+        return wordcount
 
     def lettercount(self):
-        i = 0
+        lettercount = 0
         input = self._sentence
         input_split = input.split(' ')
         for word in input_split:
-            i = i + len(word)
-        return i
+            lettercount = lettercount + len(word)
+        return lettercount
