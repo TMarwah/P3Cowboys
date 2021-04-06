b8c64db72d664d94e325901e31fee0f89522f470
KaramAlshaikh <72892221+KaramAlshaikh@users.noreply.github.com>
2021-04-05 21:14:18 -0700

    fixed the errors with the mini lab (should be able to be shown on the website)

--- Cowboys/Karam/app.py
+++ Cowboys/Karam/app.py
@@ -5,8 +5,11 @@ from .karamminilab import Caracal
 Cowboys_Karam_bp = Blueprint('Cowboys_Karam', __name__,
                                template_folder='templates',
                                static_folder='static', static_url_path='assets')
-
+@Cowboys_Karam_bp.route("/")
+def upload():
+    return render_template ("homepage2.html")
 
 @Cowboys_Karam_bp.route("/karamminilab")
 def minilab():
-    return render_template("karamminilab.html", Caracal=Caracal())
+    caracals= [Caracal("skinny", "beige"), Caracal("fat", "brown")]
+    return render_template("karamminilab.html", caracals=caracals)
