ac74e13426fb736bb58a017ca4f47bf4149b62f7
allenyxu <72947445+allenyxu@users.noreply.github.com>
2021-05-31 18:03:13 -0700

    Update model.py

--- Cowboys/Allen/model.py
+++ Cowboys/Allen/model.py
@@ -3,9 +3,7 @@ from .db import db
 
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Text, nullable=False)
+    website = db.Column(db.Text, nullable=False)
     content = db.Column(db.Text, nullable=True)
     img = db.Column(db.Text, unique=False, nullable=True)
     filename = db.Column(db.Text, nullable=False)
-    satisfaction = db.Column(db.Text)
-    mimetype = db.Column(db.Text, nullable=False)
