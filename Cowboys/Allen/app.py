e7fef8fdfbfef5be292186d53e6c5c4ead338d60
marchumeau <72947138+marchumeau@users.noreply.github.com>
2021-06-16 18:13:51 -0700

    uploading

--- Cowboys/Allen/app.py
+++ Cowboys/Allen/app.py
@@ -9,13 +9,13 @@ from .db import db, db_init
 #with open('cowboys.allen.config.json') as file:
     #config = json.load(file)
 
-app = Flask(__name__)
+#app = Flask(__name__)
 # SQLAlchemy config. Read more: https://flask-sqlalchemy.palletsprojects.com/en/2.x/
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db_init(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#db_init(app)
 
-db = SQLAlchemy()
+#db = SQLAlchemy()
 
 backgrounds = ["https://www.teahub.io/photos/full/193-1933361_laptop-aesthetic-wallpapers-anime.jpg"]
 
@@ -39,81 +39,36 @@ def browse():
     #reviews = []
 
     #for review in review_query:
-        #websiteurl = url_for('get_img', id=review.id)
 
         #review_dict = {
-            #'id': review.id,
-            #'username': review.username,
+            #'website': review.website,
             #'content': review.content,
-            #'image':  websiteurl
         #}
         #reviews.append(review_dict)
     #return render_template("browse.html", reviews=reviews, background=random.choice(backgrounds))
 
-@Cowboys_minilab1_bp.route('/upload', methods=["POST", 'GET'])
-def upload():
-    background = random.choice(backgrounds)
-    if request.method == "POST":
-        name = request.form["username"]
-        content = request.form["content"]
-        image = request.files.get('img')
-        if not image:
-            return 'bad news ur image did not make it to our servers :((((', 400
+#@Cowboys_minilab1_bp.route('/upload', methods=["POST", 'GET'])
+#def upload():
+    #background = random.choice(backgrounds)
+    #if request.method == "POST":
+        #name = request.form["website"]
+        #content = request.form["content"]
 
-        filename = secure_filename(image.filename)
-        mimetype = image.mimetype
-        if not filename or not mimetype:
-            return 'Bad upload', 400
+        #review = Review(website=name, content=content)
+        #db.session.add(review)
+        #db.session.commit()
+        #return redirect(url_for("browse.html"))
+    #return render_template("upload.html", background=background)
 
-        review = Review(username=name, content=content, img=image.read(), filename=filename, mimetype=mimetype)
-        db.session.add(review)
-        db.session.commit()
-        return redirect(url_for("browse.html"))
-    return render_template("upload.html", background=background)
-
-@Cowboys_minilab1_bp.route('/images/<int:id>')
-def get_img(id):
-    img = Review.query.filter_by(id=id).first()
-    if not img:
-        return 'No img with that id', 200
-
-    return Response(img.img, mimetype=img.mimetype)
-
-@Cowboys_minilab1_bp.route('/api/review/id=<int:id>')
-def get_review(id):
-    review = Review.query.filter_by(id=id).first()
-    if review:
-        url = url_for('get_img', id=id)
-
-        review_dict = {
-            'id': review.id,
-            'username': review.username,
-            'content': review.content,
-            'satisfaction_level': review.satisfaction,
-            'image_url': config['websiteURL'] + url
-        }
-        return jsonify(review_dict)
-
-    else:
-        return Response("No review with that id ", status=400)
-
-@Cowboys_minilab1_bp.route('/api/review/all')
-def get_all_reviews():
-    review_query = Review.query.all()
-    reviews_dict = {}
-
-    for review in review_query:
-        websiteurl = url_for('get_img', id=review.id)
-        review_dict = {
-            'id': review.id,
-            'username': review.username,
-            'content': review.content,
-            'satisfaction': review.satisfaction,
-            'image':  websiteurl
-        }
-        reviews_dict[review.id] = review_dict
-
-    return jsonify(reviews_dict)
+@Cowboys_minilab1_bp.route("/upload", methods=["POST", "GET"])
+def feedback():
+    if request.method == 'POST':
+        name = request.form.get('website')
+        comment = request.form.get('content')
+        input1 = name
+        input2 = comment
+        return render_template("browse.html", name=name, comment=comment, input1=input1, input2=input2)
+    return render_template("upload.html")
 
 
 #@Cowboys_minilab1_bp.route("/upload")
